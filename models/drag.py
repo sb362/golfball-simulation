f4c115ee3ef48348714359026f08b487368611dc
stuart <sballantyne@protonmail.com>
2019-04-09 21:05:09 +0100

    rough drag model

--- models/drag.py
+++ models/drag.py
@@ -22,15 +22,40 @@ class Golfball(simple.Golfball):
 		self._cd = cd
 
 	def pos_data(self, t0, t1):
-		x_raw, y_raw = self.__pos_data(t0, t1)
-		x_out = np.array([0])
-		y_out = np.array([0])
+		x_out = []
+		y_out = []
+		dt = 0.01
 
-		# make adjustments here
+		# displacement
+		s_x = 0
+		s_y = 0
+
+		# velocity
+		v_x = simple.horizontal(self.vi(), self.loft())
+		v_y = simple.vertical(self.vi(), self.loft())
+
+		# acceleration
+		a_x = -drag(self.area() / 2, self.cd(), v_x) / self.mass()
+		a_y = -drag(self.area() / 2, self.cd(), v_y) / self.mass() - simple.g
+
+		# calculate everything over an interval (todo: figure out how to numpy-ify this)
+		i = 0
+		while s_y >= 0:
+			s_x += dt * (v_x + a_x * dt)
+			s_y += dt * (v_y + a_y * dt)
+
+			v_x += a_x * dt
+			v_y += a_y * dt
+
+			a_x = -drag(self.area() / 2, self.cd(), v_x) / self.mass()
+			a_y = -drag(self.area() / 2, self.cd(), v_y) / self.mass() - simple.g
+
+			x_out.append(s_x)
+			y_out.append(s_y)
 
 		return x_out, y_out
 
 	# Time of flight (pos_y == 0)
 	# TODO: scipy.optimize.fsolve could be useful here for complex models
-	def tof(self):
-		return 0
+	#def tof(self):
+	#	return 0
--- plotdragtheta.py
+++ plotdragtheta.py
@@ -18,7 +18,22 @@ x, y = ball.pos_data(0, ball.tof())
 plot.plot(x, y, label=format(np.rad2deg(m.pi / 6), ".0f") + " degrees, w/o drag")
 
 
+
+ball = drag.Golfball()
+ball.setloft(np.deg2rad(10))
+ball.print()
+x, y = ball.pos_data(0, ball.tof())
+plot.plot(x, y, label=format(10.0, ".0f") + " degrees, w/ drag")
+
+ball = simple.Golfball()
+ball.setloft(np.deg2rad(10))
+ball.print()
+
+x, y = ball.pos_data(0, ball.tof())
+plot.plot(x, y, label=format(10.0, ".0f") + " degrees, w/o drag")
+
+
 plot.legend()
 plot.xlabel("x-position of ball (m)")
-plot.xlabel("y-position of ball (m)")
+plot.ylabel("y-position of ball (m)")
 plot.show()
