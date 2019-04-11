Fix calculation error

models/drag2.py
@@ -6,7 +6,7 @@ g = 9.81
 
 # F_d = 1/2 * air density * (flow velocity)^2 * coefficient of drag * surface area
 def drag(area, cd, velocity, density=1.225):
-	return 0.5 * density * (velocity ** 2) * cd * area
+	return 0.5 * density * velocity * np.abs(velocity) * cd * area
 
 
 class Golfball(simple2.Golfball):
@@ -16,7 +16,7 @@ class Golfball(simple2.Golfball):
 		self.cd = 0.3
 
 	def accelerations(self):
-		fg = np.array([0, -g])
+		fg = np.array([0, -g * self.mass])
 		fd = -drag(self.area(), self.cd, self.velocities())
 
 		return (fg + fd) / self.mass

models/simple2.py
@@ -14,11 +14,13 @@ class Golfball:
 		self.mass = 0.045
 		self.radius = 0.04267
 
+	# Reference area NOT surface area
+	# For a sphere this is the cross-sectional area
 	def area(self):
-		return 4 * np.pi * self.radius ** 2
+		return np.pi * self.radius**2
 
 	def print(self):
-		print("mass: {:.1f} kg\nradius: {:.1f} m\narea: {:.1f} m^2\nx: {:.1f} m\ny: {:.1f} m\nvx: {:.1f} m/s\nvy: {:.1f} m/s\n".format(self.mass, self.radius, self.area(), self.x, self.y, self.vx, self.vy))
+		print("mass: {:.5f} kg\nradius: {:.5f} m\narea: {:.5f} m^2\nx: {:.3f} m\ny: {:.3f} m\nvx: {:.3f} m/s\nvy: {:.3f} m/s\n".format(self.mass, self.radius, self.area(), self.x, self.y, self.vx, self.vy))
 
 	def set_coords(self, coords):
 		self.x, self.y, self.vx, self.vy = coords
@@ -34,7 +36,7 @@ class Golfball:
 
 	def accelerations(self):
 		# [x accel, y accel]
-		fg = np.array([0, -g])
+		fg = np.array([0, -g * self.mass])
 
 		return fg / self.mass
 